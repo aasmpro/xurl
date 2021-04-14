108d79fa721f4452e420983bbf0b6e343c0d88c7
Abolfazl Amiri <aa.smpro@gmail.com>
2021-04-14 17:35:42 +0430

    version: 0.0.2

--- xurl.py
+++ xurl.py
@@ -4,18 +4,22 @@ import re
 
 import requests
 
+__version__ = "0.0.2"
+
 
 def get_parser():
     parser = argparse.ArgumentParser(
         prog="xurl",
         allow_abbrev=False,
         description="extract links from file or url to a file",
+        epilog="version {}".format(__version__),
     )
     parser.add_argument("location", help="file or url", metavar="LOCATION")
     parser.add_argument("-a", action="store", type=str, help="append URL to links", metavar="URL")
     parser.add_argument("-c", action="append", type=str, help="contain text (REGEX)", metavar="TEXT")
     parser.add_argument("-C", action="append", type=str, help="not contain text (REGEX)", metavar="TEXT")
     parser.add_argument("-q", action="store_true", help="quiet mode (do not print Errors/Warnings/Infos)")
+    parser.add_argument("-v", action="version", version='%(prog)s ' + __version__)
 
     return parser
 
@@ -51,6 +55,9 @@ def main():
     args = parser.parse_args()
     quiet = args.q
     links = get_links(get_content(args.location, quiet))
+    append = args.a or args.location
+    if not append.endswith("/"):
+        append = append + "/"
     if links:
         for link in links:
             link = link.strip()
@@ -68,7 +75,7 @@ def main():
             if not accepted:
                 continue
             elif link:
-                print("{}{}".format(args.a if args.a and not link.startswith("http") else "", link))
+                print("{}{}".format(append if not link.startswith("http") else "", link))
 
     else:
         if not quiet:
